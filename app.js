5906058e0fb91baa4cbbbd34a290f3e1af121021
Catherine Meyers <ccmeyers@gmail.com>
2014-08-24 16:35:16 -0400

    adds lesson 3 - forms

--- app.js
+++ app.js
@@ -4,35 +4,53 @@
           // AngularJS   (App Name,  Dependencies)
 
   app.controller('StoreController', function(){
-    this.products = gems;
+    this.products = [
+      {
+        name: 'Awesome Multi-touch Keyboard',
+        price: 250.00,
+        description: "...",
+        reviews: [
+          {
+            stars: 5,
+            body: "I love this product!",
+            author: "joe@thomas.com"
+          },
+          {
+            stars: 1,
+            body: "This product sucks",
+            author: "tim@hater.com"
+          }
+        ]
+      }
+    ];
   });
 
-  var gems = [
-  {
-    name: 'Dodecahedron',
-    price: 2,
-    description: '...',
-    canPurchase: true,
-    soldOut: false
-  },
-  {
-    name: "Pentagonal Gem",
-    price: 5.95,
-    description: "...",
-    canPurchase: true,
-    soldOut: false,
-    // images: [
-    //   {
-    //     full: 'dodecahedron-01-full.jpg',
-    //     thumb: 'dodecahedron-01-thumb.jpg'
-    //   },
-    //   {
-    //     full: 'dodecahedron-02-full.jpg',
-    //     thumb: 'dodecahedron-02-thumb.jpg'
-    //   },
-    // ]
-  }
-  ];
+  // var gems = [
+  // {
+  //   name: 'Dodecahedron',
+  //   price: 2,
+  //   description: '...',
+  //   canPurchase: true,
+  //   soldOut: false
+  // },
+  // {
+  //   name: "Pentagonal Gem",
+  //   price: 5.95,
+  //   description: "...",
+  //   canPurchase: true,
+  //   soldOut: false,
+  //   // images: [
+  //   //   {
+  //   //     full: 'dodecahedron-01-full.jpg',
+  //   //     thumb: 'dodecahedron-01-thumb.jpg'
+  //   //   },
+  //   //   {
+  //   //     full: 'dodecahedron-02-full.jpg',
+  //   //     thumb: 'dodecahedron-02-thumb.jpg'
+  //   //   },
+  //   // ]
+  // }
+  // ];
 
   app.controller("PanelController", function(){
     this.tab = 1;
@@ -41,6 +59,15 @@
     };
     this.isSelected = function(checkTab) {
       return this.tab === checkTab;
+    };
+  });
+
+  app.controller("ReviewController", function(){
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review); //pushes review into that product's reviews array
+      this.review = {}; //clears form
     };
   });
 
