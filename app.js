d2ea1b672cd58d55a8d87650e85384067c3ebfd7
Catherine Meyers <ccmeyers@gmail.com>
2014-08-24 17:55:16 -0400

    adds lesson 4 - custom directives

--- app.js
+++ app.js
@@ -71,4 +71,32 @@
     };
   });
 
+  // custom element directive
+  app.directive('productTitle', function(){
+    return {
+      restrict: 'E', //type of directive (E for element)
+      templateUrl: 'product-title.html' //Url of a template
+    };
+  });
+
+  // if we wanted to use attribute directive
+  // app.directive('productTitle', function(){
+  //   return {
+  //     restrict: 'A', //(A for attribute)
+  //     templateUrl: 'product-title.html'
+  //   };
+  // });
+  // would put <h3 product-title></h3> in index.html
+
+  app.directive('productPanels', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller:function(){
+
+      },
+      controllerAs: 'panels'
+    };
+  });
+
 })();
